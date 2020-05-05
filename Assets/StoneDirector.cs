de0996d35664c4a86ce3a36d835049c7aab9b00f
Takumi0310 <takumiotsuka2@yahoo.com>
2020-05-05 23:25:35 +0900

    add setter for stoneRenderer

--- Assets/StoneDirector.cs
+++ Assets/StoneDirector.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class StoneDirector : MonoBehaviour
 {
     FieldData[,] fieldData = new FieldData[10,10];
-
+    StoneRenderer stoneRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                fieldData[i, j] = new FieldData();
+            }
+        }
+        stoneRenderer = GameObject.Find("Stage").GetComponent<StoneRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fieldData[3, 3].StoneState = 1;
+            stoneRenderer.FieldData=fieldData;
+
+        }
     }
 
 
--- Assets/StoneRenderer.cs
+++ Assets/StoneRenderer.cs
@@ -7,23 +7,25 @@ public class StoneRenderer : MonoBehaviour
 {
     public GameObject blackStone;
     public GameObject whiteStone;
-    public FieldData[,] fieldData = new FieldData[10,10];
+    private FieldData[,] fieldData = new FieldData[10,10];
     StageLine stageLine;
     Vector3 vector;
+
     // Start is called before the first frame update
     void Start()
     {
         for(int i = 0; i < 10; i++)
+
         {
             for(int j = 0; j < 10; j++)
             {
-                fieldData[i, j] = new FieldData();
+                FieldData[i, j] = new FieldData();
             }
         }
-        fieldData[4, 4].StoneState = 1;
-        fieldData[5, 5].StoneState = 1;
-        fieldData[5, 4].StoneState = 2;
-        fieldData[4, 5].StoneState = 2;
+        FieldData[4, 4].StoneState = 1;
+        FieldData[5, 5].StoneState = 1;
+        FieldData[5, 4].StoneState = 2;
+        FieldData[4, 5].StoneState = 2;
         stageLine = GetComponent<StageLine>();
 
         
@@ -34,6 +36,15 @@ public class StoneRenderer : MonoBehaviour
     void Update()
     {
 
+    }
+    public FieldData[,] FieldData
+    {
+        get => fieldData;
+        set
+        {
+            fieldData = value;
+            updateStoneRender();
+        }
     }
 
     public void updateStoneRender()
@@ -46,13 +57,13 @@ public class StoneRenderer : MonoBehaviour
             {
                 vector = new Vector3((stageLine.sizex / 2) - ((stageLine.gapy * (j - 1)) + 0.3f), (stageLine.sizey / 2) - ((stageLine.gapy * (i - 1)) + 0.3f));
 
-                if (fieldData[i, j].StoneState == 1)
+                if (FieldData[i, j].StoneState == 1)
                 {
                     blackStone.transform.position = vector;
                     Instantiate(blackStone, transform);
                 }
 
-                if (fieldData[i, j].StoneState == 2)
+                if (FieldData[i, j].StoneState == 2)
                 {
                     whiteStone.transform.position = vector;
                     Instantiate(whiteStone, transform);
